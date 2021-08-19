d9e77c364f09b4a2624b0a37e93642fe40e073d3
rizaldysdwinata <89172960+rizaldysdwinata@users.noreply.github.com>
2021-08-19 09:30:05 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,7 +10,7 @@ from telegram import ParseMode, BotCommand
 def start(update, context):
     LOGGER.info('UID: {} - UN: {} - MSG: {}'.format(update.message.chat.id,update.message.chat.username,update.message.text))
     if update.message.chat.type == "private" :
-        sendMessage(f"Hey <b>{update.message.chat.first_name}</b>. Welcome to <b>LoaderX Bot</b>", context.bot, update)
+        sendMessage(f"Hey <b>{update.message.chat.first_name}</b>. Welcome to <b>Google Drive Search Bot</b>", context.bot, update)
     else :
         sendMessage("Am alive :)", context.bot, update)
 
@@ -19,7 +19,7 @@ def log(update, context):
     sendLogFile(context.bot, update)
 
 botcmnd = [
-  BotCommand(f'{BotCommands.ListCommand}', ' 🔎 Search on team drive✨'),
+  BotCommand(f'{BotCommands.ListCommand}', ' 🔎 Search files on drive'),
   BotCommand(f'{BotCommands.LogCommand}','📄 Get Logs [owner only]'),
 
   ]
